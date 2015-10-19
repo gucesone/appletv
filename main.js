8412f26d66e52409c420ce6c871d3d08376e5940
gucesone <Samayoa@julios-MacBook-Pro.local>
2015-10-19 10:09:39 -0400

    new
    test

---
+++ main.js
@@ -0,0 +1,38 @@
+App.onLaunch = function(options) {
+    var mainURL = 'https://db.tt/rYiUsNJX';
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.responseType = "document";
+    xmlhttp.onload = function () {
+      navigationDocument.pushDocument(xmlhttp.responseXML);
+    };
+    xmlhttp.open("GET", mainURL, true);
+    xmlhttp.send();
+}
+
+App.onExit = function() {
+    console.log('App finished');
+}
+
+/*
+function getDocument(url) {
+    var templateXHR = new XMLHttpRequest();
+    templateXHR.responseType = "document";
+    templateXHR.addEventListener("load", function() {pushDoc(templateXHR.responseXML);}, false);
+    templateXHR.open("GET", url, true);
+    templateXHR.send();
+    return templateXHR;
+}
+
+function pushDoc(document) {
+    navigationDocument.pushDocument(document);
+}
+
+App.onLaunch = function(options) {
+    var templateURL = 'https://db.tt/rYiUsNJX';
+    getDocument(templateURL);
+}
+
+App.onExit = function() {
+    console.log('App finished');
+}
+*/
